a0c7da8661f0325e4a6ea024478647b742b3d666
harrisonchun <h7chun@uwaterloo.ca>
2020-08-24 19:37:33 -0300

    Fixed buggers

--- client/src/components/CollabPage/RoomSettings/RoomSettingsWindow.jsx
+++ client/src/components/CollabPage/RoomSettings/RoomSettingsWindow.jsx
@@ -41,11 +41,7 @@ const RoomSettingsWindow = (props) => {
     }
 
     const capacityOptions = [
-        { value: 1, label: 1 },
-        { value: 2, label: 2 },
-        { value: 3, label: 3 },
-        { value: 4, label: 4 },
-        { value: 5, label: 5 }
+        // TODO: harrison
     ]
     const capacityDefaultValue = {
         value: props.room.numMaxGuests,
